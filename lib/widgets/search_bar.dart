47d86e30585eb74e39472abceb7a91b194a787d4
Sidharth K V <sidharthkv1100@gmail.com>
2026-01-02 12:40:02 +0530

    Create search_bar.dart

---
+++ lib/widgets/search_bar.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class CustomSearchBar extends StatelessWidget {
+  const CustomSearchBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(10),
+      child: TextField(
+        decoration: InputDecoration(
+          hintText: "Search Amazon",
+          prefixIcon: const Icon(Icons.search),
+          filled: true,
+          fillColor: Colors.white,
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(8),
+            borderSide: BorderSide.none,
+          ),
+        ),
+      ),
+    );
+  }
+}
