885533f5d7f1d731c5c91538b35eca3f84593579
Sidharth K V <sidharthkv1100@gmail.com>
2026-01-02 12:40:44 +0530

    Create product_model.dart

---
+++ lib/models/product_model.dart
@@ -0,0 +1,11 @@
+class Product {
+  final String title;
+  final String image;
+  final double price;
+
+  Product({
+    required this.title,
+    required this.image,
+    required this.price,
+  });
+}
