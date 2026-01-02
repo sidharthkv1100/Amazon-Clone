426b593e4913cefc7a2bb204306b3e14352f4bad
Sidharth K V <sidharthkv1100@gmail.com>
2026-01-02 12:40:59 +0530

    Create dummy_products.dart

---
+++ lib/data/dummy_products.dart
@@ -0,0 +1,24 @@
+import '../models/product_model.dart';
+
+final List<Product> products = [
+  Product(
+    title: "iPhone 15",
+    image: "https://via.placeholder.com/150",
+    price: 799.99,
+  ),
+  Product(
+    title: "Samsung Galaxy S23",
+    image: "https://via.placeholder.com/150",
+    price: 699.99,
+  ),
+  Product(
+    title: "Sony Headphones",
+    image: "https://via.placeholder.com/150",
+    price: 199.99,
+  ),
+  Product(
+    title: "Apple Watch",
+    image: "https://via.placeholder.com/150",
+    price: 299.99,
+  ),
+];
